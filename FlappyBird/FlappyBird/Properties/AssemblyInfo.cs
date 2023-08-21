e917a2df4a40439b1cbc1e538582b41925113ffc
Mike Karnaukhov <115707524+Suduppa@users.noreply.github.com>
2023-08-21 14:24:17 +0500

    Code

---
+++ FlappyBird/FlappyBird/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FlappyBird")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FlappyBird")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80a32571-a520-4988-a83d-0f4c1a4c2fe5")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
